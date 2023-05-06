6f47c0adf53657ab3f72ac6770dacd6c306584ca
Yuri Edward <yuri6037@outlook.com>
2023-05-06 09:58:35 +0200

    Added support for sounds

---
+++ engine/src/main/java/com/github/yuri6037/sje2d/asset/factory/SoundLoader.java
@@ -0,0 +1,65 @@
+// Copyright (c) 2023, SJE2D
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of BlockProject 3D nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package com.github.yuri6037.sje2d.asset.factory;
+
+import com.github.yuri6037.sje2d.asset.Sound;
+import com.github.yuri6037.sje2d.asset.engine.AssetURL;
+import com.github.yuri6037.sje2d.asset.engine.map.AssetDepMap;
+import com.github.yuri6037.sje2d.asset.engine.system.stream.IAssetStream;
+import com.github.yuri6037.sje2d.asset.engine.system.stream.StreamUtils;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
+public final class SoundLoader extends BaseLoader<Sound> {
+    private final InputStream stream;
+    private Clip clip;
+
+    SoundLoader(final IAssetStream stream, final AssetURL url) {
+        super(url);
+        this.stream = new BufferedInputStream(StreamUtils.makeInputStream(stream));
+    }
+
+    @Override
+    public Result load(final AssetDepMap dependencies) throws Exception {
+        AudioInputStream audio = AudioSystem.getAudioInputStream(stream);
+        clip = AudioSystem.getClip();
+        clip.open(audio);
+        return Result.ready();
+    }
+
+    @Override
+    protected Sound createAsset() throws Exception {
+        return new Sound(clip);
+    }
+}
