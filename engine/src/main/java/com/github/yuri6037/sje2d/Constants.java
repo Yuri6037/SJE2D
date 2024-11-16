4ee297bc0a5156d584e5c8f47beff129edc629f9
Yuri Edward <yuri6037@outlook.com>
2024-11-16 14:35:51 +0100

    Initiated version 2.0-SNAPSHOT

--- engine/src/main/java/com/github/yuri6037/sje2d/Constants.java
+++ engine/src/main/java/com/github/yuri6037/sje2d/Constants.java
@@ -34,7 +34,7 @@ public final class Constants {
     /**
      * The engine version.
      */
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "2.0-SNAPSHOT";
 
     private Constants() {
     }
