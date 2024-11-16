14143cf537d18ae5a017a00b0c6b865b421af106
Yuri Edward <yuri6037@outlook.com>
2024-11-16 14:23:54 +0100

    Stabilized version of SJE2D

--- engine/src/main/java/com/github/yuri6037/sje2d/Constants.java
+++ engine/src/main/java/com/github/yuri6037/sje2d/Constants.java
@@ -34,7 +34,7 @@ public final class Constants {
     /**
      * The engine version.
      */
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.0";
 
     private Constants() {
     }
